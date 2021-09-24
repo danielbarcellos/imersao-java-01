0d92dea8c734c2e252a0cfa7585abeb01e2cddaa
Daniel Ribeiro Barcellos <daniel.barcellos@eldorado.org.br>
2021-09-24 14:39:20 -0300

    first commit

---
+++ src/main/java/com/example/imersaojava01/ImersaoJava01Application.java
@@ -0,0 +1,13 @@
+package com.example.imersaojava01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImersaoJava01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImersaoJava01Application.class, args);
+	}
+
+}
